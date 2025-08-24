7e5c0bf2e89ee27ac712572366a5988910d358f4
p627997 <p627997@gmail.com>
2025-08-24 19:26:30 +0530

    Sentiment Analysis

---
+++ sentiment-analysis.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+
+texts= [
+    "I love this phone, the camera quality is amazing!",
+    "This movie was so boring and too long.",
+    "I ordered the book yesterday and it arrived today."
+]
+
+for sentence in texts:
+    blob = TextBlob(sentence)
+    sentiment = blob.sentiment.polarity
+
+    if sentiment > 0:
+        result = "Positive"
+    elif sentiment < 0:
+        result = "Negative"
+    else:
+        result = "Neutral"
+
+    print(f"Text: {sentence}\nSentiment:{result}\n")
